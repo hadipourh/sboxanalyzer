82848488555b27bf3375a49abf36de9e87a72393
hadipourh <hsn.hadipour@gmail.com>
2025-10-27 22:22:33 +0100

    Bump version to 1.0.2

--- sboxanalyzer/__init__.py
+++ sboxanalyzer/__init__.py
@@ -69,7 +69,7 @@ EXAMPLES::
 # In case you use this tool please include the above copyright informations (name, contact, license)
 #*****************************************************************************
 
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 
 import subprocess
 import os
